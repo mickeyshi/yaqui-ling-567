62c6ed5e31e0abfd5bd0b096bdd679d3f248aea9
Mickey Shi <bigmigs@uw.edu>
2021-02-13 13:14:11 -0800

    Overhauled verb lexical classes and morphology

--- Version.lsp
+++ Version.lsp
@@ -1,4 +1,4 @@
 ;;; -*- Mode: TDL; Coding: utf-8 -*-
 (in-package :common-lisp-user)
 
-(defparameter *grammar-version* "Hiaki (2021-02-04_21:22:06_UTC)")
+(defparameter *grammar-version* "Hiaki (2021-02-12_04:55:04_UTC)")
--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -21,8 +21,6 @@
 
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
-((:path . "corpus") (:content . "IGT provided by the linguist"))
-((:path . "lab4") (:content . "Test suite collected for Lab2 2-4."))
 ; New test suites here. For example:
 ; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
 )
